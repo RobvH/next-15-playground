754b4c4304f4c9730ee93ccf0b208ca8d75571aa
Rob van Hoose <robertbvanhoose@gmail.com>
2025-04-30 03:12:54 -0400

    feat: add server rendered data page

---
+++ src/app/server-data/page.tsx
@@ -0,0 +1,23 @@
+type User = {
+    id: number
+    name: string
+    username: string
+}
+
+async function getUsers(): Promise<User[]> {
+    const request = await fetch('https://jsonplaceholder.typicode.com/users')
+
+    return await request.json()
+}
+
+export default async function ServerDataPage() {
+    const users = await getUsers()
+
+    return (
+        <>
+            <h1>Server Data Page</h1>
+
+            {users.map((user: User) => <div key={user.id}>{user.username}</div>)}
+    </>
+)
+}
